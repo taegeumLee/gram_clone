a0c56882d985f0da481ac16734dcedc84d8f91b7
taegeumLee <xorma443101@gmail.com>
2024-11-14 16:04:59 +0900

    restart

---
+++ app/(auth)/components/BackgroundSlider.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import Image from "next/image";
+import { motion } from "framer-motion";
+
+export default function BackgroundSlider() {
+  const [imageIndex, setImageIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setImageIndex((prevIndex) => (prevIndex + 1) % 5);
+    }, 10000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <motion.div
+      key={imageIndex}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 3, ease: "easeInOut" }}
+      className="absolute inset-0"
+    >
+      <Image
+        src={`/images/background/${imageIndex}.jpeg`}
+        alt="Background"
+        fill
+        className="object-cover brightness-[0.7]"
+        priority
+      />
+    </motion.div>
+  );
+}
---
+++ app/fonts/fonts.ts
@@ -0,0 +1,7 @@
+import { Pacifico } from "next/font/google";
+
+export const pacifico = Pacifico({
+  subsets: ["latin"],
+  variable: "--font-pacifico",
+  weight: "400",
+});
